9417fa928905979544a0c8591e8ff59c14d74538
Raushan Raj <86942178+rraushan1205@users.noreply.github.com>
2023-06-18 10:19:36 +0530

    index update

---
+++ script.js
@@ -0,0 +1,13 @@
+function changeImage1() {
+    document.getElementById("mainimgs").src = "https://drive.google.com/uc?export=download&id=12E_H5HXhNwvdNS4Uq2XkbX_dWcbtAUy4";
+}
+
+function changeImage2() {
+    document.getElementById("mainimgs").src = "https://drive.google.com/uc?export=download&id=1BbMcsIv8RxWbOZZminz23Zoas9wqtzRB";
+}
+
+function changeImage3() {
+    document.getElementById("mainimgs").src = "https://drive.google.com/uc?export=download&id=1MKh3-1ukEkyCZ3g0Hahoawq4Sy6vLH6Z";
+}
+
+  
